047b55207e69cdac7c4ec111d7a78f5ec3f19bdc
Vitali Nartov <nartov@axinom.com>
2015-09-22 19:42:57 +0300

    code clean up logo update

--- app/src/main/java/com/divapps/aipok/devclub/MainActivity.java
+++ app/src/main/java/com/divapps/aipok/devclub/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,40 +19,6 @@ public class MainActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         if(ab != null) {
             ab.setTitle(null);
-//            Drawable drawable;
-//            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
-//                drawable = getResources().getDrawable(R.drawable.action_bar_bg, getTheme());
-//            else
-//                drawable = getResources().getDrawable(R.drawable.action_bar_bg);
-//            if(drawable != null) {
-//                Drawable dr = drawable.mutate();
-//                dr.setAlpha(150);
-//                ab.setBackgroundDrawable(dr);
-//            }
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_reload) {
-            FeedListFragment fragment = (FeedListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
-            fragment.reloadFeeds();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
